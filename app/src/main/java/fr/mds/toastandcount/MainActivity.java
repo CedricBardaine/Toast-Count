71291b92392029d36a5ad01370fa2539fe8b0937
Cédric B <cedricbardaine@gmail.com>
2020-03-16 14:58:59 +0100

    manage save on rotation

--- app/src/main/java/fr/mds/toastandcount/MainActivity.java
+++ app/src/main/java/fr/mds/toastandcount/MainActivity.java
@@ -1,8 +1,10 @@
 package fr.mds.toastandcount;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.mShowCount = (TextView) findViewById(R.id.TV_show_count) ;
+        this.mShowCount = (TextView) findViewById(R.id.TV_show_count);
     }
 
     public void countUp(View view) {
@@ -27,4 +29,23 @@ public class MainActivity extends AppCompatActivity {
     public void showToast(View view) {
         Toast.makeText(this , "hey, that's a toast !"  , Toast.LENGTH_SHORT).show();
     }
+
+
+    // manage rotation
+
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        savedInstanceState.putInt("theCount" , this.mCount);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        this.mCount = savedInstanceState.getInt("theCount");
+        this.mShowCount.setText(Integer.toString(this.mCount));
+    }
+
+
 }
